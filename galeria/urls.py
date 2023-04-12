09c74823f6e7af9f39c718d53282e948d0342326
renagol <rras2@cesar.school>
2023-04-12 11:38:45 -0300

    Criacao de telas

--- galeria/urls.py
+++ galeria/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from galeria.views import index, imagem
+from galeria.views import index, imagem, login, cadastro, post
 
 urlpatterns = [
     path('', index, name='index'),
     path('imagem/', imagem, name='imagem'),
+    path('login/',  login, name='login'),
+    path('cadastro/', cadastro, name='cadastro'),
+    path('post/', post, name='post'),
 ]
--- galeria/views.py
+++ galeria/views.py
@@ -5,3 +5,12 @@ def index(request):
 
 def imagem(request):
     return render(request, 'galeria/imagem.html')
+
+def login(request):
+    return render(request, 'galeria/login.html')
+
+def cadastro(request):
+    return render(request, 'galeria/cadastro.html')
+
+def post(request):
+    return render(request, 'galeria/post.html')
