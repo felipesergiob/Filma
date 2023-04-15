7282e915ff078695a46643ecc861727709aea117
brunnapontual <brunnapontua@hotmail.com>
2023-04-15 16:17:10 -0300

    banco de dados filmes certo

--- galeria/models.py
+++ galeria/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 from django.contrib.auth.models import User
-from django.core.validators import MinValueValidator, MaxValueValidator
 
 class Perfil(models.Model):
     nome = models.CharField(max_length=70, null=True)
@@ -8,18 +7,16 @@ class Perfil(models.Model):
 
     def __str__(self):
         return self.nome
-
-#Adicionando filmes ao banco de dados  
-class Filme2(models.Model):
-    Titulo = models.CharField(max_length=100, null=False, blank=False)
-    Genero = models.CharField(max_length=100, null=False, blank=False)
-    Elenco = models.CharField(max_length=500)
-    Sinopse = models.TextField(max_length=1000)
-    Avaliacao = models.FloatField(default=0, validators=[MinValueValidator(0), MaxValueValidator(5)])
-    Capa = models.CharField(max_length=100, null=False, blank=False)
-    Ano_de_producao = models.DateField()
-    Status = models.CharField(max_length=100)
-    Views_count = models.IntegerField(default=0)
+    
+#Adicionando filmes ao banco de dados
+class Filme(models.Model):
+    titulo = models.CharField(max_length=100, null=False, blank=False)
+    genero = models.CharField(max_length=100, null=False, blank=False)
+    elenco = models.CharField(max_length=100)
+    sinopse = models.TextField(max_length=1000)
+    capa = models.CharField(max_length=100, null=False, blank=False)
+    ano_de_producao = models.DateField()
+    views_count = models.IntegerField(default=0)
 
     def __str__(self):
-        return f"Filme [Titulo={self.Titulo}]"
+        return self.titulo
