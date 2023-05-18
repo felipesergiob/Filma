e4a183b090d0263ae3ccbc21ad3436501aa28c8e
renagol <rras2@cesar.school>
2023-05-17 21:08:24 -0300

    Revert "deploy 1"
    This reverts commit 2e1b0623b2c45db7bec12df0c20c88b48b792cb4.

--- setup/settings.py
+++ setup/settings.py
@@ -78,12 +78,13 @@ WSGI_APPLICATION = 'setup.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-DATABASES = { 
-		'default': {
-		'ENGINE': 'utils.db.backends.sqlite3', 
-		'NAME': BASE_DIR / 'db.sqlite3', 
-		}
-	}
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
@@ -119,8 +120,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_ROOT = 'static'
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'setup/static')
@@ -141,4 +141,3 @@ MEDIA_URL = '/media/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
