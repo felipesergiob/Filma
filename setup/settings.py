3c6d1df0edd157a8b32d308b9b00f140aea62f19
felipesergiob <fssmbf@cesar.school>
2023-06-12 17:50:19 -0300

    allowed hosts for deploy

--- setup/settings.py
+++ setup/settings.py
@@ -141,3 +141,10 @@ MEDIA_URL = '/media/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+ALLOWED_HOSTS = [
+    # other allowed hosts
+    '15.229.164.250',
+    'localhost',
+    '127.0.0.1',
+]
